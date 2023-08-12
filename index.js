f1a7baf3fd99237c757f6f4a055eca29758c01ae
MisterCow1 <scott@colquhoun.com>
2023-08-12 23:40:24 +0100

    Removed main.old.js

--- index.js
+++ index.js
@@ -1,52 +1,82 @@
+class Square {
+  constructor(row, col) {
+    this.row = row
+    this.col = col
+    this.index = 8 * row + col
+    this.piece = ""
+  }
+
+  set(piece) {
+    this.piece = piece
+    document.getElementsByClassName("square")[this.index].innerHTML = "<img src='svgs/" + this.piece + ".svg'>"
+  }
+}
+
 class Board {
   constructor() {
-
+    this.squares = []
   }
 
   createBoard() {
     const boardContainer = document.getElementById("board-container")
 
-    for(let i = 0; i < 8; i++) {
+    for (let i = 0; i < 8; i++) {
       let boardRow = document.createElement("div")
       boardRow.classList.add("board-row")
       boardContainer.appendChild(boardRow)
 
-      for(let j = 0; j < 8; j++) {
-        let square = document.createElement("square")
-        const index = 8*i + j
+      this.squares[i] = []
+
+      for (let j = 0; j < 8; j++) {
+        let square = document.createElement("div")
+        const index = 8 * i + j
         square.id = index
-        square.innerText = " "
         square.classList.add("square")
-        
+
+        this.squares[i][j] = new Square(i, j)
+
         const jOffset = i % 2 ? j + 1 : j
         const isWhiteSquare = jOffset % 2 === 0
         square.classList.add(isWhiteSquare ? "white-square" : "black-square")
 
         boardRow.appendChild(square)
-      
-        square.addEventListener("click", () => { onSquareClicked(i, j) })
-      } 
+
+        square.addEventListener("click", () => { this.onSquareClicked(i, j) })
+      }
     }
   }
 
   setSquare(row, col, to) {
-    const index = 8*row + col
-    const square = document.getElementsByClassName("square")[index]
-    square.innerText = to
+    const square = this.squares[row][col]
+    square.set(to)
   }
 
   getSquare(row, col) {
-    const index = 8*row + col
-    const square = document.getElementsByClassName("square")[index]
-    return square.innerText
+    const square = this.squares[row][col]
+    return square.id
   }
 
   getBoard() {
+    let board = []
 
+    for (let i = 0; i < 8; i++) {
+      board[i] = []
+
+      for (let j = 0; j < 8; j++) {
+        board[i][j] = this.squares[i][j].piece
+      }
+    }
+
+    return board
   }
 
   onSquareClicked(row, col) {
-
+    console.log("Square " + row + " " + col + " clicked")
+    //const index = 8 * row + col
+    //const squares = document.getElementsByClassName("square")
+    //let clickedSquare = squares[index]
+    //clickedSquare.classList.add("clicked")
+    //clickedSquare.innerHTML = "<img src='svgs/p.svg'>"
   }
 }
 
@@ -54,9 +84,8 @@ window.onload = function() {
   const board = new Board()
   board.createBoard()
 
-  board.setSquare(0, 0, "P")
-  board.setSquare(5, 0, "5")
-  board.setSquare(0, 5, "f")
+  board.setSquare(0, 5, "p")
+  board.setSquare(7, 2, "K")
 
-  console.log(board.getSquare(0, 5))
+  console.log(board.getBoard())
 }
